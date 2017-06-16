c779f787ee4c3e8cd88c162b24bde6ce85eed98d
Oouchi Tomoyuki <toouchi@nekonet.co.jp>
2017-06-16 12:13:01 +0900

    add coment

--- gituser_set.sh
+++ gituser_set.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
+# global user set
 echo -n "global user.name : "
 read g_user
 git config --global user.name "$g_user"
+
+# global user email set
 echo -n "global user.Email : "
 read g_email
 git config --global user.email "$g_email"
+
+# global user push set
 echo -n "global push.default(simple or current) : "
 read p_default
 git config --global push.default "$p_default"
+
+# local user set(git update user)
 echo -n "local user.name : "
 read l_user
 git config --local user.name "$l_user"
+
+# local user email(git update user email)
 echo -n "local user.Email : "
 read l_email
 git config --local user.email "$l_email"
